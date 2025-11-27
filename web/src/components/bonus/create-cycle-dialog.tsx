4dd7e9558b256b2102087f7258fab7df056501c4
Somprasong Damyos <somprasong.damyos@gmail.com>
2025-11-27 11:07:38 +0700

    fix: use `value` prop for Select components in bonus cycle creation dialog.

--- web/src/components/bonus/create-cycle-dialog.tsx
+++ web/src/components/bonus/create-cycle-dialog.tsx
@@ -234,7 +234,7 @@ export function CreateCycleDialog({ onSuccess }: CreateCycleDialogProps) {
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>{t('payrollMonth')}</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <Select onValueChange={field.onChange} value={field.value}>
                       <FormControl>
                         <SelectTrigger>
                           <SelectValue />
@@ -258,7 +258,7 @@ export function CreateCycleDialog({ onSuccess }: CreateCycleDialogProps) {
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>{t('payrollYear')}</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <Select onValueChange={field.onChange} value={field.value}>
                       <FormControl>
                         <SelectTrigger>
                           <SelectValue />
